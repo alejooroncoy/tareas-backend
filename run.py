912cf8d32489d3566e56124d58dd99496516d979
César Mayta <cesarmayta@gmail.com>
2022-11-21 22:27:46 -0500

    deploy 3

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 app.app_context().push()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cmaytausrtareas:sv5BoD7q7XT29yGxL2PtBQ6LBLmoKyFu@dpg-cdu3rg1a6gdv3spdf9i0-a.oregon-postgres.render.com/cmaytadbtareas'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://cmaytausrtareas:sv5BoD7q7XT29yGxL2PtBQ6LBLmoKyFu@dpg-cdu3rg1a6gdv3spdf9i0-a.oregon-postgres.render.com/cmaytadbtareas'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
